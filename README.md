ea19fabf69ff6d670b09a2ae565773abf89a0fda
Johnathan Silva <jsrgyn@gmail.com>
2021-01-26 14:15:02 -0300

    Iniciando - Parei com 23 min

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # JSExpert
+
 &lt;Semana/> JS expert - https://youtu.be/zAEu5Smx5h4
+https://javascriptexpert.com.br/lc_jse_jan21?utm_source=outros_org&utm_campaign=lc_jse_jan21 (https://bit.ly/js-expert-week02-wa)
+Github: https://github.com/ErickWendel/jsexpert02-skeleton-ew
+
+---
+
+Dia 25/01/2021
+#1 - Estruturando o seu projeto Zoom:
+3 Design Patterns que você precisa conhecer -
+#SemanaJSExpert
+
+---
+
+npm ci --silent
+npm start
+https://giphy.com/gifs/espn-football-alabama-nationalchampionship-SLv4ETnzGZRWi3hyaN/links
+https://media.giphy.com/media/SLv4ETnzGZRWi3hyaN/giphy.mp4
+Parei com 23min
+================================================================================
---
+++ aula01/pages/room/src/app.js
@@ -0,0 +1,24 @@
+
+
+const recordClick = function (recorderBtn) {
+  this.recordingEnabled = false
+  return () => {
+    this.recordingEnabled = !this.recordingEnabled
+    recorderBtn.style.color = this.recordingEnabled ? 'red' : 'white'
+  }
+}
+
+const onload = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const room = urlParams.get('room');
+  console.log('this is the room', room)
+
+  // const recorderBtn = document.getElementById('record')
+  // recorderBtn.addEventListener('click', recordClick(recorderBtn))
+
+  const view = new View() 
+  view.renderVideo({ userId: 'test01', url: 'https://media.giphy.com/media/SLv4ETnzGZRWi3hyaN/giphy.mp4 '})
+
+}
+
+window.onload = onload
